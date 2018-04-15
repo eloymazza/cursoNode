063fb40d8dc2454bef8e21e3fcfe514272cb8650
Eloy Mazza <eloymazza.ra@gmail.com>
2018-04-15 20:39:24 -0300

    Hice hasta ejercicio 19

---
+++ 02.fundamentos/arrow-functions.js
@@ -0,0 +1,11 @@
+
+let saludar = () => {
+    let a = 1;
+    let b = 2;
+    return a+b;
+};
+
+let saludar2 = (nombre) => `hola ${nombre}`;
+
+console.log(saludar("eloy"));
+console.log(saludar2("eloy"));
---
+++ 02.fundamentos/callback.js
@@ -0,0 +1,26 @@
+//setTimeout(()=> console.log("hola mundo"),3000);
+
+let getUsuarioByID = (id,callback) => {
+    // Suposicion de que voy a la base de datos
+    let usuario = {
+        nombre: 'Fernando',
+    //si el nombre es igual al contenido se puede poner asi:
+        id  
+    }
+    if(id === 20){
+        callback(`El usuario con id ${id} no existe en la base de datos`)
+    }
+    else{
+        callback(undefined, usuario);
+    }
+}
+
+getUsuarioByID(20, (err,usuario)=>{
+
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Usuario de base de datos", usuario); 
+    }
+});
